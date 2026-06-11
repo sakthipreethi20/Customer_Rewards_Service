baffd449c40ab37f74a25a9d556fd5f5d3d8f232
sakthipreethi <144205618+sakthipreethi20@users.noreply.github.com>
2026-06-11 12:52:32 +0530

    Update data.sql

--- reward-service/src/main/resources/data.sql
+++ reward-service/src/main/resources/data.sql
@@ -1,22 +1,22 @@
 INSERT INTO transactions VALUES
-(1,  101, 'Sravani Bulle',    120.00, DATEADD('MONTH', -2, CURRENT_DATE)),
-(2,  101, 'Sravani Bulle',     75.00, DATEADD('MONTH', -1, CURRENT_DATE)),
-(3,  101, 'Sravani Bulle',    200.00, CURRENT_DATE),
+(1, 101, 'Sakthi', 120.00, DATEADD('MONTH', -2, CURRENT_DATE)),
+(2, 101, 'Sakthi', 75.00, DATEADD('MONTH', -1, CURRENT_DATE)),
+(3, 101, 'Sakthi', 200.00, CURRENT_DATE),
 
-(4,  102, 'Sri Lakshmi',        130.00, DATEADD('MONTH', -2, CURRENT_DATE)),
-(5,  102, 'Sri Lakshmi',         90.00, DATEADD('MONTH', -1, CURRENT_DATE)),
-(6,  102, 'Sri Lakshmi',        180.00, CURRENT_DATE),
+(4, 102, 'Preethi', 130.00, DATEADD('MONTH', -2, CURRENT_DATE)),
+(5, 102, 'Preethi', 90.00, DATEADD('MONTH', -1, CURRENT_DATE)),
+(6, 102, 'Preethi', 180.00, CURRENT_DATE),
 
-(7,  103, 'Latha Sri',        45.00, DATEADD('MONTH', -2, CURRENT_DATE)),
-(8,  103, 'Latha Sri',        60.00, DATEADD('MONTH', -1, CURRENT_DATE)),
-(9,  103, 'Latha Sri',       140.00, CURRENT_DATE),
+(7, 103, 'Sakthi', 45.00, DATEADD('MONTH', -2, CURRENT_DATE)),
+(8, 103, 'Sakthi', 60.00, DATEADD('MONTH', -1, CURRENT_DATE)),
+(9, 103, 'Sakthi', 140.00, CURRENT_DATE),
 
-(10, 104, 'Roopa',         49.00, CURRENT_DATE),
-(11, 104, 'Roopa',         51.00, CURRENT_DATE),
-(12, 104, 'Roopa',         99.00, CURRENT_DATE),
-(13, 104, 'Roopa',        101.00, CURRENT_DATE),
+(10, 104, 'Preethi', 49.00, CURRENT_DATE),
+(11, 104, 'Preethi', 51.00, CURRENT_DATE),
+(12, 104, 'Preethi', 99.00, CURRENT_DATE),
+(13, 104, 'Preethi', 101.00, CURRENT_DATE),
 
-(14, 105, 'Mahitha',      250.00, CURRENT_DATE),
-(15, 105, 'Mahitha',      300.00, DATEADD('MONTH', -1, CURRENT_DATE)),
+(14, 105, 'Sakthi', 250.00, CURRENT_DATE),
+(15, 105, 'Sakthi', 300.00, DATEADD('MONTH', -1, CURRENT_DATE)),
 
 (16, 106, 'OldCustomer', 500.00, DATEADD('MONTH', -6, CURRENT_DATE));
